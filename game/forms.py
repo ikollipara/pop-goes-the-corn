e9d801c70e8236a26f1013996134fee4bcafa5b8
Reagan Zierke <reaganzierke@gmail.com>
2025-03-01 13:24:10 -0600

    login fomr

--- game/forms.py
+++ game/forms.py
@@ -5,3 +5,10 @@ Ian Kollipara <ian.kollipara@cune.edu>
 
 Forms Baby :)
 """
+
+from django import forms
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    display_name = forms.CharField(max_length=255)
--- game/views.py
+++ game/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .models import User
+from .forms import LoginForm
 
-# Create your views here.
+
+def home(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            display_name = form.cleaned_data["display_name"]
+
+            user = User.objects.filter(email=email).first()
+            if user:
+                login(request, user)
+            else:
+                user = User.objects.create(email=email, display_name=display_name)
+                login(request, user)
+            return redirect("home")
+    else:
+        form = LoginForm()
+
+    return render(request, "home.html", {"form": form})
