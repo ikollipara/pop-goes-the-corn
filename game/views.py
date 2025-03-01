1e80fd9e9c1f8a72055ab8a2ab063b1c53fe95fd
Reagan Zierke <71458762+Galacticica@users.noreply.github.com>
2025-03-01 14:02:48 -0600

    Login view (#1)
    * login fomr
    
    * Updated code styling
    
    * fixed login issue
    
    ---------
    
    Co-authored-by: Ian Kollipara <ian.kollipara@gmail.com>

--- game/views.py
+++ game/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from django.views import View
 
-# Create your views here.
+from .forms import UserLoginForm
+from .models import User
+
+
+class HomeView(View):
+    def get(self, request):
+        form = UserLoginForm()
+        return render(request, "game/login.html", {"form": form})
+
+    def post(self, request):
+        form = UserLoginForm(request.POST, self.request.FILES)
+        if form.is_valid():
+            user, _ = form.save()
+
+            response = redirect("home")
+            response.set_cookie("email", user.email)
+            return response
